3401c5ec039715d011a09dcb4de0a19dab8bc587
Igor <igor.xot@mail.ru>
2022-12-31 00:33:45 +0300

    dz9

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------DZ9-------------------------------------------------------------------
+
+//  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+/*
+
+ void ShowNums(int num)
+{   
+    Console.Write(num + " ");
+    if(num > 1)
+    {
+        ShowNums(num - 1);
+    }
+            
+}
+   
+Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ShowNums(n);
+
+*/
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+/*
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натур элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+GapNumberSum(numberM, numberN,0);
+
+*/
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+/*
+
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
+
+*/
